1badb0a10f28c94b3b4995640a1fec399e3e5a78
cdaringe <christopher.dieringer@gmail.com>
2017-03-22 17:57:32 -0700

    :art: promise API only

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ var set = require('lodash/set')
 var saucie = require('saucie')
 var pidFile = 'sc_client.pid'
 
-module.exports = function (config, cb) {
+module.exports = function (config) {
   if (!config) throw new Error('missing config')
-  set(config, 'plugins.pre.saucie', function sauciePre (config, cb) {
+  set(config, 'pre.saucie', function sauciePre (config) {
     var opts = {
       username: process.env.SAUCE_USERNAME,
       accessKey: process.env.SAUCE_ACCESS_KEY,
@@ -18,14 +18,9 @@ module.exports = function (config, cb) {
     var buildNumber = process.env.TRAVIS_JOB_NUMBER || process.env.CI_BUILD_NUMBER || process.env.BUILD_NUMBER
     if (buildNumber) opts.tunnelIdentifier = buildNumber
 
-    saucie.connect(opts)
-    .then(function preOK () { cb() })
-    .catch(cb)
+    return saucie.connect(opts)
   })
-  set(config, 'plugins.post.saucie', function sauciePost (config, cb) {
-    saucie.disconnect(pidFile)
-    .then(function postOK () { cb() })
-    .catch(cb)
+  set(config, 'post.saucie', function sauciePost (config) {
+    return saucie.disconnect(pidFile)
   })
-  cb()
 }
