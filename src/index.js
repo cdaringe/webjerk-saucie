06ac8625dcd7c6dad1884e5f123b2aa36714a632
cdaringe <christopher.dieringer@gmail.com>
2017-03-31 17:01:01 -0700

    0.0.3

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
 'use strict'
 
-var set = require('lodash/set')
 var saucie = require('saucie')
 var pidFile = 'sc_client.pid'
 
-module.exports = function (config) {
-  if (!config) throw new Error('missing config')
-  set(config, 'pre.saucie', function sauciePre (config) {
-    var opts = {
-      username: process.env.SAUCE_USERNAME,
-      accessKey: process.env.SAUCE_ACCESS_KEY,
-      verbose: true,
-      logger: console.log,
-      pidfile: pidFile
+module.exports = function (webjerkconfig) {
+  return {
+    name: 'webjerk-saucie',
+    pre (pluginConfig, webjerkconfig) {
+      var SAUCE_USERNAME = process.env.SAUCE_USERNAME
+      var SAUCE_ACCESS_KEY = process.env.SAUCE_ACCESS_KEY
+      if (!SAUCE_ACCESS_KEY || !SAUCE_USERNAME) throw new Error('SAUCE_ACCESS_KEY & SAUCE_USERNAME must be provided')
+      var opts = {
+        username: SAUCE_USERNAME,
+        accessKey: SAUCE_ACCESS_KEY,
+        verbose: true,
+        logger: console.log,
+        pidfile: pidFile
+      }
+
+      var buildNumber = process.env.TRAVIS_JOB_NUMBER || process.env.CI_BUILD_NUMBER || process.env.BUILD_NUMBER
+      if (buildNumber) opts.tunnelIdentifier = buildNumber
+
+      return saucie.connect(opts)
+    },
+    post (pluginConfig, webjerkconfig) {
+      return saucie.disconnect(pidFile)
     }
-
-    var buildNumber = process.env.TRAVIS_JOB_NUMBER || process.env.CI_BUILD_NUMBER || process.env.BUILD_NUMBER
-    if (buildNumber) opts.tunnelIdentifier = buildNumber
-
-    return saucie.connect(opts)
-  })
-  set(config, 'post.saucie', function sauciePost (config) {
-    return saucie.disconnect(pidFile)
-  })
+  }
 }
